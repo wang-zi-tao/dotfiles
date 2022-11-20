7ce284b3b76f2933607bf1c58d7149e9e107022b
wang-zi-tao <2451355023@qq.com>
2022-11-20 13:58:32 +0800

    完善nixOnDroid配置

--- nix-on-droid/mobile.nix
+++ nix-on-droid/mobile.nix
@@ -1,15 +1,40 @@
-inputs@{ pkgs, lib, config, ... }: {
-  imports = [ ../module/nix.nix ];
+inputs@{ pkgs, lib, config, nixpkgs, nur, nixpkgs-unstable, ... }: {
   environment.packages = with pkgs;[
     gnugrep
     gnused
     command-not-found
   ];
-  environment.etcBackupExtension = ".bak";
   home-manager.config = { ... }: (import ../home-manager/profiles/wangzi-mini.nix (inputs // { inherit pkgs; })).configuration;
   home-manager.useGlobalPkgs = true;
   user.shell = "${pkgs.zsh}/bin/zsh";
   system.stateVersion = "22.05";
   terminal.font = "${pkgs.nerdfonts}/share/fonts/truetype/NerdFonts/Iosevka Nerd Font Complete Mono.ttf";
   time.timeZone = "Asia/Shanghai";
+
+  nix = {
+    substituters = [
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+    ];
+    trustedPublicKeys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+    extraOptions = "experimental-features = nix-command flakes";
+    nixPath = [ "nixpkgs=${nixpkgs}" ];
+    registry.n.flake = nixpkgs;
+    registry.nixpkgs.flake = nixpkgs;
+    registry.u.flake = nixpkgs-unstable;
+    registry.unstable.flake = nixpkgs-unstable;
+    registry.nur.flake = nur;
+  };
+  nixpkgs.config.allowUnfree = true;
+  time.timeZone = "Asia/Shanghai";
+  environment.etc.nixos = {
+    source = ../.;
+  };
 }
