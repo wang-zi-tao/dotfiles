d72d48e9b94f6e1cca346adc3622851003310e3c
wang-zi-tao <2451355023@qq.com>
2024-09-20 23:22:08 +0800

    andriod 设备上关闭atuin

--- home-manager/profiles/wangzi-mobile.nix
+++ home-manager/profiles/wangzi-mobile.nix
@@ -10,4 +10,5 @@
   home.username = "nix-on-droid";
   home.homeDirectory = "/data/data/com.termux.nix/files/home";
   programs.zsh.enableCompletion = lib.mkForce false;
+  programs.atuin.enable = lib.mkForce false;
 }
