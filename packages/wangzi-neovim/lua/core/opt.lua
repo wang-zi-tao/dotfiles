14020e049264260f83cecd7be51ba268edfdb68c
wang-zi-tao <2451355023@qq.com>
2025-09-03 19:36:11 +0800

    auto commit

--- packages/wangzi-neovim/lua/core/opt.lua
+++ packages/wangzi-neovim/lua/core/opt.lua
@@ -101,7 +101,6 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     pattern = { "*.cpp", "*.c", "*.cc", "*.inl", "CMakeLists.txt", "*.h", "*.hpp", "*.pch", "*.def" },
     callback = function()
-        opt.filetype = "cpp"
         opt.tabstop = 4
         opt.softtabstop = 0
         opt.shiftwidth = 4
@@ -110,6 +109,13 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
         opt.autoindent = true
         opt.expandtab = false
         -- vim.cmd [[set bomb]]
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+    pattern = { "*.cpp", "*.c", "*.cc", "*.inl", "*.h", "*.hpp", "*.pch", "*.def" },
+    callback = function()
+        opt.filetype = "cpp"
     end,
 })
 
