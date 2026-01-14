7d32b4d1a2bfda3d4617ceed3587c5c240aebfe6
wang-zi-tao <2451355023@qq.com>
2026-01-14 17:57:44 +0800

    auto commit

--- packages/wangzi-neovim/skeleton/shell.nix
+++ packages/wangzi-neovim/skeleton/shell.nix
@@ -5,6 +5,18 @@ let
   fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") { };
 in
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [ ];
+  nativeBuildInputs = with pkgs; [ 
+	# cmake
+    # clang
+	# clang-tools
+  ];
+  buildInputs = with pkgs; [ 
+
+  ];
   shellHook = "";
+  NIX_LD = "${pkgs.llvmPackages_18.libllvm.dev}/bin/ld.lld";
+  NIX_LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+
+  ];
+
 }
